3b9c532bd28e728475051daeb2b474fc552bec7a
Ajaleang <ajaleang@unal.edu.co>
2025-04-05 00:05:36 -0500

    para deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/portafolio-hv/', // este nombre debe coincidir con el de tu repositorio en GitHub
   plugins: [react()],
-})
+});
