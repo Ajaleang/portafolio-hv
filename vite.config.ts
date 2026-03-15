f22f89cd19515ba3ce5c442266662cf2892f95a4
Ajaleang <ajaleang@unal.edu.co>
2026-03-14 20:11:08 -0500

    fix: ajustar base path para GitHub Pages en lugar de Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/', // 👈 esto es clave para Vercel
+  base: '/portafolio-hv/', // 👈 esto es clave para GitHub Pages (nombre del repositorio)
   plugins: [react()],
 });
