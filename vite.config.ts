f0496fde1a489f94d25bf767fc32caf3bea73abb
Ajaleang <ajaleang@unal.edu.co>
2026-03-14 20:11:26 -0500

    eliminar comentario innecesario

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/portafolio-hv/', // 👈 esto es clave para GitHub Pages (nombre del repositorio)
+  base: '/portafolio-hv/',
   plugins: [react()],
 });
